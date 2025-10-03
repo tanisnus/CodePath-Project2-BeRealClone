438a39ce14dc442968fa5ff88d56dcf3c5825505
tanisnus <taniss246@gmail.com>
2025-10-02 20:56:59 -0700

    Initial Commit

---
+++ CodePath-Project2-BeRealClone/CodePath_Project2_BeRealCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodePath_Project2_BeRealCloneApp.swift
+//  CodePath-Project2-BeRealClone
+//
+//  Created by Tanis Sarbatananda on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct CodePath_Project2_BeRealCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodePath-Project2-BeRealClone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CodePath-Project2-BeRealClone
+//
+//  Created by Tanis Sarbatananda on 10/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
